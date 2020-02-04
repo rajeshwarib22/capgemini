a462271eea7f0efae0a3b741352c7d057e097a33
rajeshwarib22 <57486845+rajeshwarib22@users.noreply.github.com>
2020-02-04 15:29:42 +0530

    Add files via upload

---
+++ junitdemo/src/main/java/junitdemo/SampleTest.java
@@ -0,0 +1,23 @@
+package junitdemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void SimpleTestPass() {
+		int a=1;
+		int b=2;
+		assertTrue(a+b==3);
+	}
+	
+	@Test
+	public void SimpleTestFail() {
+		int a=1;
+		int b=2;
+		assertTrue(a+b==3);
+	}
+
+}
